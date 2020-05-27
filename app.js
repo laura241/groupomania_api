a18d6ba7a40a05e0601e2fd1ffa5d00bf9fe9975
laura241 <laura241@protonmail.com>
2020-05-27 18:57:25 +0200

    add express

--- app.js
+++ app.js
@@ -2,3 +2,11 @@ const express = require("express");
 
 //Initialisation of the app
 const app = express();
+
+app.use((req, res) => {
+    res.json({
+        message: 'Votre requête a bien été reçue'
+    })
+});
+
+module.exports = app;
