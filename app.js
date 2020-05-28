256c9454a7c9b4b561f0acb35b1389f4d23e8aa4
laura241 <laura241@protonmail.com>
2020-05-28 17:48:03 +0200

    add connection to mysql bdd

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require("express");
+const mysql = require('mysql');
 
 //Initialisation of the app
 const app = express();
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    port: 8889,
+    password: "root",
+    database: "groupomania_bdd"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
 
 app.use((req, res) => {
     res.json({
