62f26f41a7b49a069efc002c151bb607794f43b5
laura241 <laura241@protonmail.com>
2020-07-01 17:48:54 +0200

    add getAllMessages controllers

--- controllers/message.js
+++ controllers/message.js
@@ -1,36 +1,31 @@
 const db = require("../config/db");
 const Message = db.messages;
-const {
-    authJwt
-} = require("../middlewares")
 
 exports.newPost = (req, res) => {
-    Message.create({
-            message: req.body.message,
-            userId: req.body.userId
-        })
-        .then(() => {
-            return res.status(201).send({
-                message: 'The post was sent successfully'
-            });
-        })
-        .catch(err => {
-            return res.status(500).send({
-                message: err.message || "Some error occured while creating the message"
-            })
-        });
-}
+  Message.create({
+      message: req.body.message,
+      userId: req.body.userId,
+    })
+    .then(() => {
+      return res.status(201).send({
+        message: "The post was sent successfully",
+      });
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: err.message || "Some error occured while creating the message",
+      });
+    });
+};
 
 exports.getAllMessages = (req, res) => {
-    Message.findAll({
-            raw: true
-        })
-        .then((data) => {
-            return data
-        })
-        .catch(err => {
-            return res.status(500).send({
-                message: err.message || "Some error occured"
-            })
-        })
-}
+  Message.findAll()
+    .then(messages => {
+      return res.status(200).send(messages)
+    })
+    .catch((err) => {
+      return res.status(500).send({
+        message: err.message || "Some error occured",
+      });
+    });
+};
